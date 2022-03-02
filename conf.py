dd7c284fe621ab9410ca13442a537d9fcaacde80
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-03-02 11:59:28 -0500

    creating skel

--- conf.py
+++ conf.py
@@ -36,7 +36,7 @@ release = '202202152222'
 extensions = [
     "sphinxcontrib.youtube",
     "sphinx_copybutton",
-    "sphinx-tabs"
+    "sphinx_tabs.tabs"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
